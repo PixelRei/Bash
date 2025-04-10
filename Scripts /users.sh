973fdf282f27726b7fb92d5198bd9e57f0ae025f
PixelRei <playboi.rei17@gmail.com>
2025-04-10 22:46:17 +0200

    Create users.sh

---
+++ Scripts /users.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ (id -i) -eq 0 ] ; then
+read -p “Inserisci username: “ username
+read -s -p “Inserisci password: “ password
+grep -q $username /etc/passwd
+if [ $? -eq 1 ]; then
+useradd -m “$username” -p “$password”
+[ $? -eq 0 ] && echo -e “\nutente aggiunto” || echo -e “n\No”
+else
+echo -e “\nUsername $username già presente!”
+exit 1
+fi
+else
+echo “Attenzione! Solo root può aggiungere un utente al sistema”
+exit 2
+fi
